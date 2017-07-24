035b10839d8691a55b0269527f3b51958af404fd
VP Nagraj <vpnagraj@virginia.edu>
2017-07-24 10:06:59 -0400

    setup() and dependencies() now from package not predefined in app

--- inst/bootstrapr_app/server.R
+++ inst/bootstrapr_app/server.R
@@ -1,39 +1,5 @@
 library(shiny)
-
-dependencies <- function(deps = NULL, update = TRUE) {
-
-  # combine specified deps with baseline
-  deps <- c(deps,
-            "r-base",
-            "libapparmor1",
-            "libcurl4-gnutls-dev",
-            "libxml2-dev",
-            "libssl-dev",
-            "gdebi-core",
-            "libcairo2-dev",
-            "libxt-dev",
-            "git-core")
-
-  res <- sapply(deps,
-                function(x) paste0("\nsudo apt-get -y install ", x, collapse = "\n"))
-
-  if(update) {
-    # cat(paste0("sudo apt-get update ",res, collapse = "\n"))
-    # cat(c("sudo apt-get update\n",res))
-    c("sudo apt-get update\n",res)
-
-  } else {
-    # cat(res)
-    res
-  }
-
-}
-
-setup <- function() {
-
-  "sudo sh -c 'echo 'deb http://cran.rstudio.com/bin/linux/ubuntu trusty/' >> /etc/apt/sources.list'\ngpg --keyserver keyserver.ubuntu.com --recv-key E084DAB\ngpg -a --export E084DAB9 | sudo apt-key add -\n"
-
-}
+library(bootstrapr)
 
 shinyServer(function(input, output) {
 
