6660f99b5d73751ca8bdd9cabb1b1bcd24987dce
VP Nagraj <vpnagraj@virginia.edu>
2017-09-22 14:14:19 -0400

    tests for shiny and rstudio args

--- tests/testthat/test_bootstrapr.R
+++ tests/testthat/test_bootstrapr.R
@@ -5,3 +5,15 @@ test_that("user cannot specify package not in CRAN", {
   expect_error(bootstrapr(cran = "thisisnotapackage"))
 
 })
+
+context("dependencies")
+
+test_that("rstudio server and shiny server include args work", {
+
+  isthere <- grepl("rstudio-server", bootstrapr("dplyr", rstudio = FALSE, write = F))
+  expect_false(isthere)
+
+  isthere <- grepl("shiny-server", bootstrapr("dplyr", shiny = FALSE, write = F))
+  expect_false(isthere)
+
+})
