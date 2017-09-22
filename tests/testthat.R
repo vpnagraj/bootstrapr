7fe0f3eeea715d28d68abcdce94f45c08bdd4b18
VP Nagraj <vpnagraj@virginia.edu>
2017-09-22 15:29:47 -0400

    resolved issue with mirror option in tests and added new test

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,9 @@
 library(testthat)
 library(bootstrapr)
 
+# need to set option for cran repo for testthat
+r <- getOption("repos")
+r["CRAN"] <- "https://cran.rstudio.com"
+options(repos = r)
+
 test_check("bootstrapr")
--- tests/testthat/test_bootstrapr.R
+++ tests/testthat/test_bootstrapr.R
@@ -6,14 +6,21 @@ test_that("user cannot specify package not in CRAN", {
 
 })
 
+test_that("package appears in bootstrapping script", {
+
+  isthere <- grepl("dplyr", bootstrapr(cran = "dplyr", write = FALSE))
+  expect_true(isthere)
+
+})
+
 context("dependencies")
 
 test_that("rstudio server and shiny server include args work", {
 
-  isthere <- grepl("rstudio-server", bootstrapr(rstudio_server = FALSE, write = F))
+  isthere <- grepl("rstudio-server", bootstrapr(rstudio_server = FALSE, write = FALSE))
   expect_false(isthere)
 
-  isthere <- grepl("shiny-server", bootstrapr(shiny_server = FALSE, write = F))
+  isthere <- grepl("shiny-server", bootstrapr(shiny_server = FALSE, write = FALSE))
   expect_false(isthere)
 
 })
