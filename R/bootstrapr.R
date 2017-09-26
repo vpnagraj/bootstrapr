f3ca68518416d05ae93041df349080b33c6d4bc7
VP Nagraj <vpnagraj@virginia.edu>
2017-09-26 13:52:19 -0400

    removed file extension parameter for bootstrapped script

--- R/bootstrapr.R
+++ R/bootstrapr.R
@@ -9,13 +9,12 @@
 #' @param cran character vector indicating the packages to be installed from \url{https://cran.r-project.org/}
 #' @param bioc character vector indicating the packages to be installed from \url{http://bioconductor.org/}
 #' @param write logical indicating whether or not to write the bootstrapping statement to a file
-#' @param file_name name of file to be written; defaults to "bootstrap"
-#' @param file_ext character vector of length 1 indicating file extension to use; defaults to ".sh"
+#' @param file_name name of file to be written; defaults to "bootstrap.sh"
 #' @param rstudio_server logical indicating whether or not to include a RStudio Server
 #' @param shiny_server logical indicating whether or not to include a Shiny Server
 #' @param cran_repo a character vector of length 1 specifying the CRAN mirror from which you want to install the packages; defaults to user's CRAN mirror global option
 
-bootstrapr <- function(cran = NULL, bioc = NULL, write = FALSE, file_name = "bootstrap", file_ext = "sh", rstudio_server = TRUE, shiny_server = TRUE, cran_repo = getOption("repos")["CRAN"]) {
+bootstrapr <- function(cran = NULL, bioc = NULL, write = FALSE, file_name = "bootstrap.sh", rstudio_server = TRUE, shiny_server = TRUE, cran_repo = getOption("repos")["CRAN"]) {
 
   script <-
     c(setup(),
@@ -24,17 +23,12 @@ bootstrapr <- function(cran = NULL, bioc = NULL, write = FALSE, file_name = "boo
       rstudio_server(rstudio_server),
       shiny_server(shiny_server))
 
-
-  if(file_ext == "sh") {
-
-    script <- paste0(c("#!/bin/sh", script), collapse = "\n")
-
-  }
+  script <- paste0(c("#!/bin/sh", script), collapse = "\n")
 
   if(write) {
 
     write(paste0(script, collapse = "\n"),
-          file = paste0(c(file_name, ".", file_ext), collapse = ""),
+          file = file_name,
           append = FALSE)
 
   }
